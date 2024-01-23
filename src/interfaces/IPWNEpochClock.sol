4dd14f77e56a93d9090ef2c0964fb34900b587b0
ashhanai <ashhab.naim@gmail.com>
2024-01-23 15:57:36 -0100

    Add missing docs to IPWNEpochClock

--- src/interfaces/IPWNEpochClock.sol
+++ src/interfaces/IPWNEpochClock.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
+/// @title Interface of the PWN Epoch Clock contract.
+/// @notice The contract is used to get the current epoch number.
 interface IPWNEpochClock {
 
     /// @notice Returns the current epoch number.
