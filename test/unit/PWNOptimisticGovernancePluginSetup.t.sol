218c8cf8d562aee1f43fdcb67a20875d5d524257
ashhanai <ashhab.naim@gmail.com>
2024-01-30 15:25:40 -0100

    Unit test pwn optimistic governance plugin setup

--- test/unit/PWNOptimisticGovernancePluginSetup.t.sol
+++ test/unit/PWNOptimisticGovernancePluginSetup.t.sol
@@ -1,25 +1,26 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.17;
 
-import { IPWNTokenGovernance } from "src/governance/token/IPWNTokenGovernance.sol";
+import { IPWNOptimisticGovernance } from "src/governance/optimistic/IPWNOptimisticGovernance.sol";
 import {
-    PWNTokenGovernancePlugin,
-    PWNTokenGovernancePluginSetup,
+    PWNOptimisticGovernancePlugin,
+    PWNOptimisticGovernancePluginSetup,
     PermissionLib,
     IPluginSetup
-} from "src/governance/token/PWNTokenGovernancePluginSetup.sol";
+} from "src/governance/optimistic/PWNOptimisticGovernancePluginSetup.sol";
 
 import { Base_Test, Vm } from "../Base.t.sol";
 
-abstract contract PWNTokenGovernancePluginSetup_Test is Base_Test {
+abstract contract PWNOptimisticGovernancePluginSetup_Test is Base_Test {
 
     address public dao = makeAddr("dao");
     address public epochClock = makeAddr("epochClock");
     address public votingToken = makeAddr("votingToken");
     bytes32 public DUMMY_EXECUTE_PERMISSION_ID = keccak256("DUMMY_EXECUTE_PERMISSION_ID");
 
-    PWNTokenGovernancePluginSetup public pluginSetup;
-    PWNTokenGovernancePlugin.TokenGovernanceSettings public governanceSettings;
+    PWNOptimisticGovernancePluginSetup public pluginSetup;
+    PWNOptimisticGovernancePlugin.OptimisticGovernanceSettings public governanceSettings;
+    address[] public proposers;
 
     function setUp() public virtual {
         vm.mockCall(
@@ -28,15 +29,16 @@ abstract contract PWNTokenGovernancePluginSetup_Test is Base_Test {
             abi.encode(DUMMY_EXECUTE_PERMISSION_ID)
         );
 
-        pluginSetup = new PWNTokenGovernancePluginSetup();
+        pluginSetup = new PWNOptimisticGovernancePluginSetup();
 
-        governanceSettings = PWNTokenGovernancePlugin.TokenGovernanceSettings({
-            votingMode: IPWNTokenGovernance.VotingMode.Standard,
-            supportThreshold: 0,
-            minParticipation: 0,
-            minDuration: 1 hours,
-            minProposerVotingPower: 0
+        governanceSettings = PWNOptimisticGovernancePlugin.OptimisticGovernanceSettings({
+            minVetoRatio: 1,
+            minDuration: 4 days
         });
+
+        proposers.push(makeAddr("proposer1"));
+        proposers.push(makeAddr("proposer2"));
+        proposers.push(makeAddr("proposer3"));
     }
 
 }
@@ -46,10 +48,10 @@ abstract contract PWNTokenGovernancePluginSetup_Test is Base_Test {
 |*  # CONSTRUCTOR                                           *|
 |*----------------------------------------------------------*/
 
-contract PWNTokenGovernancePluginSetup_Constructor_Test is PWNTokenGovernancePluginSetup_Test {
+contract PWNOptimisticGovernancePluginSetup_Constructor_Test is PWNOptimisticGovernancePluginSetup_Test {
 
-    function test_shouldDeployTokenGovernancePluginBase() external {
-        pluginSetup = new PWNTokenGovernancePluginSetup();
+    function test_shouldDeployOptimisticGovernancePluginBase() external {
+        pluginSetup = new PWNOptimisticGovernancePluginSetup();
 
         assertNotEq(pluginSetup.implementation(), address(0));
     }
@@ -61,16 +63,27 @@ contract PWNTokenGovernancePluginSetup_Constructor_Test is PWNTokenGovernancePlu
 |*  # PREPARE INSTALLATION                                  *|
 |*----------------------------------------------------------*/
 
-contract PWNTokenGovernancePluginSetup_PrepareInstallation_Test is PWNTokenGovernancePluginSetup_Test {
+contract PWNOptimisticGovernancePluginSetup_PrepareInstallation_Test is PWNOptimisticGovernancePluginSetup_Test {
 
     bytes public installParameters;
 
     function setUp() public override {
         super.setUp();
 
-        installParameters = pluginSetup.encodeInstallationParams(governanceSettings, epochClock, votingToken);
+        installParameters = pluginSetup.encodeInstallationParams(
+            governanceSettings, epochClock, votingToken, proposers
+        );
     }
 
+
+    function test_shouldFail_whenNoProposers() external {
+        installParameters = pluginSetup.encodeInstallationParams(
+            governanceSettings, epochClock, votingToken, new address[](0)
+        );
+
+        vm.expectRevert(abi.encodeWithSelector(PWNOptimisticGovernancePluginSetup.NoProposers.selector));
+        pluginSetup.prepareInstallation(dao, installParameters);
+    }
 
     function test_shouldReturnNewlyDeployedAndInitializedPluginClone() external {
         vm.recordLogs();
@@ -78,16 +91,13 @@ contract PWNTokenGovernancePluginSetup_PrepareInstallation_Test is PWNTokenGover
         Vm.Log[] memory logs = vm.getRecordedLogs();
 
         Vm.Log memory log = logs[1];
-        assertEq(log.topics[0], keccak256("TokenGovernanceSettingsUpdated(uint8,uint32,uint32,uint64,uint256)"));
+        assertEq(log.topics[0], keccak256("OptimisticGovernanceSettingsUpdated(uint32,uint64)"));
         assertEq(
             keccak256(log.data),
             keccak256(
                 abi.encode(
-                    uint8(governanceSettings.votingMode),
-                    governanceSettings.supportThreshold,
-                    governanceSettings.minParticipation,
-                    governanceSettings.minDuration,
-                    governanceSettings.minProposerVotingPower
+                    governanceSettings.minVetoRatio,
+                    governanceSettings.minDuration
                 )
             )
         );
@@ -104,12 +114,12 @@ contract PWNTokenGovernancePluginSetup_PrepareInstallation_Test is PWNTokenGover
         assertEq(log.emitter, plugin);
     }
 
-    function test_shouldGrantPermission_UPDATE_TOKEN_GOVERNANCE_SETTINGS_PERMISSION_ID_wherePlugin_whoDAO() external {
+    function test_shouldGrantPermission_UPDATE_OPTIMISTIC_GOVERNANCE_SETTINGS_PERMISSION_ID_wherePlugin_whoDAO() external {
         (address plugin, IPluginSetup.PreparedSetupData memory preparedSetupData)
             = pluginSetup.prepareInstallation(dao, installParameters);
 
         PermissionLib.MultiTargetPermission memory permission = preparedSetupData.permissions[0];
-        bytes32 permissionId = PWNTokenGovernancePlugin(plugin).UPDATE_TOKEN_GOVERNANCE_SETTINGS_PERMISSION_ID();
+        bytes32 permissionId = PWNOptimisticGovernancePlugin(plugin).UPDATE_OPTIMISTIC_GOVERNANCE_SETTINGS_PERMISSION_ID();
         assertEq(uint8(permission.operation), uint8(PermissionLib.Operation.Grant));
         assertEq(permission.where, plugin);
         assertEq(permission.who, dao);
@@ -122,7 +132,7 @@ contract PWNTokenGovernancePluginSetup_PrepareInstallation_Test is PWNTokenGover
             = pluginSetup.prepareInstallation(dao, installParameters);
 
         PermissionLib.MultiTargetPermission memory permission = preparedSetupData.permissions[1];
-        bytes32 permissionId = PWNTokenGovernancePlugin(plugin).UPGRADE_PLUGIN_PERMISSION_ID();
+        bytes32 permissionId = PWNOptimisticGovernancePlugin(plugin).UPGRADE_PLUGIN_PERMISSION_ID();
         assertEq(uint8(permission.operation), uint8(PermissionLib.Operation.Grant));
         assertEq(permission.where, plugin);
         assertEq(permission.who, dao);
@@ -143,6 +153,21 @@ contract PWNTokenGovernancePluginSetup_PrepareInstallation_Test is PWNTokenGover
         assertEq(permission.permissionId, permissionId);
     }
 
+    function test_shouldGrantPermission_PROPOSER_PERMISSION_ID_wherePlugin_whoProposers() external {
+        (address plugin, IPluginSetup.PreparedSetupData memory preparedSetupData)
+            = pluginSetup.prepareInstallation(dao, installParameters);
+
+        for (uint256 i; i < proposers.length; ++i) {
+            PermissionLib.MultiTargetPermission memory permission = preparedSetupData.permissions[3 + i];
+            bytes32 permissionId = PWNOptimisticGovernancePlugin(plugin).PROPOSER_PERMISSION_ID();
+            assertEq(uint8(permission.operation), uint8(PermissionLib.Operation.Grant));
+            assertEq(permission.where, plugin);
+            assertEq(permission.who, proposers[i]);
+            assertEq(permission.condition, PermissionLib.NO_CONDITION);
+            assertEq(permission.permissionId, permissionId);
+        }
+    }
+
 }
 
 
@@ -150,7 +175,7 @@ contract PWNTokenGovernancePluginSetup_PrepareInstallation_Test is PWNTokenGover
 |*  # PREPARE UNINSTALLATION                                *|
 |*----------------------------------------------------------*/
 
-contract PWNTokenGovernancePluginSetup_PrepareUninstallation_Test is PWNTokenGovernancePluginSetup_Test {
+contract PWNOptimisticGovernancePluginSetup_PrepareUninstallation_Test is PWNOptimisticGovernancePluginSetup_Test {
 
     address public plugin = makeAddr("plugin");
     IPluginSetup.SetupPayload public setupPayload;
@@ -166,13 +191,13 @@ contract PWNTokenGovernancePluginSetup_PrepareUninstallation_Test is PWNTokenGov
     }
 
 
-    function test_shouldRevokePermission_UPDATE_TOKEN_GOVERNANCE_SETTINGS_PERMISSION_ID_wherePlugin_whoDAO() external {
+    function test_shouldRevokePermission_UPDATE_OPTIMISTIC_GOVERNANCE_SETTINGS_PERMISSION_ID_wherePlugin_whoDAO() external {
         PermissionLib.MultiTargetPermission[] memory permissions
             = pluginSetup.prepareUninstallation(dao, setupPayload);
 
         PermissionLib.MultiTargetPermission memory permission = permissions[0];
-        bytes32 permissionId = PWNTokenGovernancePlugin(pluginSetup.implementation())
-            .UPDATE_TOKEN_GOVERNANCE_SETTINGS_PERMISSION_ID();
+        bytes32 permissionId = PWNOptimisticGovernancePlugin(pluginSetup.implementation())
+            .UPDATE_OPTIMISTIC_GOVERNANCE_SETTINGS_PERMISSION_ID();
         assertEq(uint8(permission.operation), uint8(PermissionLib.Operation.Revoke));
         assertEq(permission.where, plugin);
         assertEq(permission.who, dao);
@@ -185,7 +210,7 @@ contract PWNTokenGovernancePluginSetup_PrepareUninstallation_Test is PWNTokenGov
             = pluginSetup.prepareUninstallation(dao, setupPayload);
 
         PermissionLib.MultiTargetPermission memory permission = permissions[1];
-        bytes32 permissionId = PWNTokenGovernancePlugin(pluginSetup.implementation())
+        bytes32 permissionId = PWNOptimisticGovernancePlugin(pluginSetup.implementation())
             .UPGRADE_PLUGIN_PERMISSION_ID();
         assertEq(uint8(permission.operation), uint8(PermissionLib.Operation.Revoke));
         assertEq(permission.where, plugin);
@@ -214,32 +239,27 @@ contract PWNTokenGovernancePluginSetup_PrepareUninstallation_Test is PWNTokenGov
 |*  # ENCODE INSTALLATION PARAMS                            *|
 |*----------------------------------------------------------*/
 
-contract PWNTokenGovernancePluginSetup_EncodeInstallationParams_Test is PWNTokenGovernancePluginSetup_Test {
+contract PWNOptimisticGovernancePluginSetup_EncodeInstallationParams_Test is PWNOptimisticGovernancePluginSetup_Test {
 
     function testFuzz_shouldReturnEncodedParams(
-        uint8 _votingMode,
-        uint32 _supportThreshold,
-        uint32 _minParticipation,
+        uint32 _minVetoRatio,
         uint64 _minDuration,
-        uint256 _minProposerVotingPower,
         address _epochClock,
         address _votingToken
     ) external {
-        governanceSettings.votingMode = IPWNTokenGovernance.VotingMode(_votingMode % 3);
-        governanceSettings.supportThreshold = _supportThreshold;
-        governanceSettings.minParticipation = _minParticipation;
+        governanceSettings.minVetoRatio = _minVetoRatio;
         governanceSettings.minDuration = _minDuration;
-        governanceSettings.minProposerVotingPower = _minProposerVotingPower;
 
         bytes memory encodedParams = pluginSetup.encodeInstallationParams(
             governanceSettings,
             _epochClock,
-            _votingToken
+            _votingToken,
+            proposers
         );
 
         assertEq(
             keccak256(encodedParams),
-            keccak256(abi.encode(governanceSettings, _epochClock, _votingToken))
+            keccak256(abi.encode(governanceSettings, _epochClock, _votingToken, proposers))
         );
     }
 
@@ -250,37 +270,33 @@ contract PWNTokenGovernancePluginSetup_EncodeInstallationParams_Test is PWNToken
 |*  # DECODE INSTALLATION PARAMS                            *|
 |*----------------------------------------------------------*/
 
-contract PWNTokenGovernancePluginSetup_DecodeInstallationParams_Test is PWNTokenGovernancePluginSetup_Test {
+contract PWNOptimisticGovernancePluginSetup_DecodeInstallationParams_Test is PWNOptimisticGovernancePluginSetup_Test {
 
     function testFuzz_shouldReturnDecodedParams(
-        uint8 _votingMode,
-        uint32 _supportThreshold,
-        uint32 _minParticipation,
+        uint32 _minVetoRatio,
         uint64 _minDuration,
-        uint256 _minProposerVotingPower,
         address _epochClock,
         address _votingToken
     ) external {
-        governanceSettings.votingMode = IPWNTokenGovernance.VotingMode(_votingMode % 3);
-        governanceSettings.supportThreshold = _supportThreshold;
-        governanceSettings.minParticipation = _minParticipation;
+        governanceSettings.minVetoRatio = _minVetoRatio;
         governanceSettings.minDuration = _minDuration;
-        governanceSettings.minProposerVotingPower = _minProposerVotingPower;
-        bytes memory encodedParams = abi.encode(governanceSettings, _epochClock, _votingToken);
+        bytes memory encodedParams = abi.encode(governanceSettings, _epochClock, _votingToken, proposers);
 
         (
-            PWNTokenGovernancePlugin.TokenGovernanceSettings memory decodedGovernanceSettings,
+            PWNOptimisticGovernancePlugin.OptimisticGovernanceSettings memory decodedGovernanceSettings,
             address decodedEpochClock,
-            address decodedVotingToken
+            address decodedVotingToken,
+            address[] memory decodedProposers
         ) = pluginSetup.decodeInstallationParams(encodedParams);
 
-        assertEq(uint8(decodedGovernanceSettings.votingMode), _votingMode % 3);
-        assertEq(decodedGovernanceSettings.supportThreshold, _supportThreshold);
-        assertEq(decodedGovernanceSettings.minParticipation, _minParticipation);
+        assertEq(decodedGovernanceSettings.minVetoRatio, _minVetoRatio);
         assertEq(decodedGovernanceSettings.minDuration, _minDuration);
-        assertEq(decodedGovernanceSettings.minProposerVotingPower, _minProposerVotingPower);
         assertEq(decodedEpochClock, _epochClock);
         assertEq(decodedVotingToken, _votingToken);
+        assertEq(decodedProposers.length, proposers.length);
+        for (uint256 i; i < decodedProposers.length; ++i) {
+            assertEq(decodedProposers[i], proposers[i]);
+        }
     }
 
 }
